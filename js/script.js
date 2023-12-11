a2455eb679638a91b50e1df74a951f83b6e373c1
muhammad fikri haikal <65498504+MEN-CLICK@users.noreply.github.com>
2023-12-11 02:06:00 -0800

    Perbaikan Penulisan

--- js/script.js
+++ js/script.js
@@ -4,11 +4,10 @@ emailField.focus({
   preventScroll: true,
 });
 function showPopup() {
-  document.getElementById('popup-container').style.display = 'block';
+  document.getElementById("popup-container").style.display = "block";
 }
-
 function closePopup() {
-  document.getElementById('popup-container').style.display = 'none';
+  document.getElementById("popup-container").style.display = "none";
 }
 // floating responsive Product cart
 function toggleNavResponsive() {
@@ -20,16 +19,13 @@ function toggleNavResponsive() {
   }
 }
 // Script JavaScript untuk toggle menu di tampilan mobile
-document.getElementById('check').addEventListener('change', function() {
-  const headerListNav = document.querySelector('.header-list-nav ul');
-  headerListNav.classList.toggle('active');
+document.getElementById("check").addEventListener("change", function () {
+  const headerListNav = document.querySelector(".header-list-nav ul");
+  headerListNav.classList.toggle("active");
 });
-
-
 
 // header Product cart
 var cart = [];
-
 
 // Fungsi untuk menampilkan dan mengupdate isi keranjang di sidenav
 function displayCart() {
@@ -40,7 +36,9 @@ function displayCart() {
     var product = cart[i];
     var productItem = document.createElement("div");
     productItem.innerHTML = `
-      <p class="cartBook">${product.name} - Rp. ${product.price.toLocaleString()}/Pax</p>
+      <p class="cartBook">${
+        product.name
+      } - Rp. ${product.price.toLocaleString()}/Pax</p>
       <button onclick="decreaseQuantity(${i})">-</button>
       <span>${product.quantity}</span>
       <button onclick="increaseQuantity(${i})">+</button>
@@ -98,64 +96,64 @@ function displayCart() {
   // Tombol "Bayar Sekarang" dengan fungsi onClick
   var payNowButton = document.createElement("button");
   payNowButton.textContent = "Pesan Sekarang";
-  payNowButton.onclick = function() {
-     // Mendapatkan data formulir
-     var name = document.getElementById("name").value;
-     var email = document.getElementById("email").value;
-     var phone = document.getElementById("phone").value;
-     var bookingDate = document.getElementById("bookingDate").value;
- 
-     // Cek apakah ada produk di keranjang
-     if (cart.length === 0) {
-       alert("Keranjang kosong. Tambahkan produk ke keranjang terlebih dahulu.");
-       return;
-     }
- 
-     // Cek apakah semua data formulir sudah diisi
-     if (!name || !email || !phone || !bookingDate) {
-       alert("Harap isi semua kolom formulir dengan benar.");
-       return;
-     }
-      // Mengumpulkan nama-nama produk di keranjang
-      var productNames = cart.map(function(product) {
-        return product.name;
+  payNowButton.onclick = function () {
+    // Mendapatkan data formulir
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var bookingDate = document.getElementById("bookingDate").value;
+
+    // Cek apakah ada produk di keranjang
+    if (cart.length === 0) {
+      alert("Keranjang kosong. Tambahkan produk ke keranjang terlebih dahulu.");
+      return;
+    }
+
+    // Cek apakah semua data formulir sudah diisi
+    if (!name || !email || !phone || !bookingDate) {
+      alert("Harap isi semua kolom formulir dengan benar.");
+      return;
+    }
+    // Mengumpulkan nama-nama produk di keranjang
+    var productNames = cart.map(function (product) {
+      return product.name;
+    });
+
+    // Data yang akan dikirim ke server
+    var bookingData = {
+      name: name,
+      email: email,
+      telepon: phone,
+      dateAt: bookingDate,
+      quantity: calculateTotalQuantity(), // Fungsi calculateTotalQuantity() untuk menghitung total jumlah produk
+      totalPrice: calculateTotal(), // Fungsi calculateTotal() untuk menghitung total harga produk
+      productName: productNames.join(", "), // Menyatukan nama-nama produk menjadi satu string
+    };
+
+    // Menggunakan metode fetch untuk melakukan permintaan POST ke API
+    fetch("https://be-2-medan-1-production.up.railway.app/booking", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bookingData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Menanggapi respons dari server
+        if (data.success) {
+          showPopup();
+          // Setelah Booking berhasil, mereset keranjang atau melakukan tindakan lainnya.
+          cart = [];
+          displayCart();
+        } else {
+          showPopup();
+        }
+      })
+      .catch((error) => {
+        console.error("Error making POST request:", error);
+        alert("Terjadi kesalahan. Silakan coba lagi nanti.");
       });
-      
-     // Data yang akan dikirim ke server
-     var bookingData = {
-       name: name,
-       email: email,
-       telepon: phone,
-       dateAt: bookingDate,
-       quantity: calculateTotalQuantity(), // Fungsi calculateTotalQuantity() untuk menghitung total jumlah produk
-       totalPrice: calculateTotal(), // Fungsi calculateTotal() untuk menghitung total harga produk
-       productName: productNames.join(", "), // Menyatukan nama-nama produk menjadi satu string
-      };
- 
-     // Menggunakan metode fetch untuk melakukan permintaan POST ke API
-     fetch("https://be-2-medan-1-production.up.railway.app/booking", {
-       method: "POST",
-       headers: {
-         "Content-Type": "application/json",
-       },
-       body: JSON.stringify(bookingData),
-     })
-       .then((response) => response.json())
-       .then((data) => {
-         // Menanggapi respons dari server
-         if (data.success) {
-          showPopup();
-           // Setelah Booking berhasil, mereset keranjang atau melakukan tindakan lainnya.
-           cart = [];
-           displayCart();
-         } else {
-          showPopup();
-         }
-       })
-       .catch((error) => {
-         console.error("Error making POST request:", error);
-         alert("Terjadi kesalahan. Silakan coba lagi nanti.");
-       });
   };
   cartContent.appendChild(payNowButton);
 }
@@ -173,8 +171,8 @@ function showNotification(message) {
 // Fungsi untuk menambahkan produk ke dalam keranjang
 function addToCart(name, price, productId) {
   // Cek apakah produk sudah ada di keranjang
-  var existingProduct = cart.find(product => product.name === name);
-  
+  var existingProduct = cart.find((product) => product.name === name);
+
   if (existingProduct) {
     // Jika produk sudah ada, tambahkan jumlahnya
     existingProduct.quantity += 1;
@@ -239,35 +237,45 @@ function toggleNav() {
 }
 
 // Paket Rekomendasi Untuk ditampilkan
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // URL API
-  const apiUrl = 'https://be-2-medan-1-production.up.railway.app/destination';
+  const apiUrl = "https://be-2-medan-1-production.up.railway.app/destination";
 
   // Tangkap elemen HTML dengan id 'destinationList'
-  const destinationListElement = document.getElementById('destinationList');
+  const destinationListElement = document.getElementById("destinationList");
 
   // Ambil data dari API
   fetch(apiUrl)
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
       return response.json();
     })
-    .then(data => {
+    .then((data) => {
       // Buat elemen HTML untuk setiap destinasi
-      data.forEach(destination => {
-        const destinationElement = document.createElement('div');
-        destinationElement.classList.add('product-cart');
+      data.forEach((destination) => {
+        const destinationElement = document.createElement("div");
+        destinationElement.classList.add("product-cart");
         destinationElement.innerHTML = `
-          <img src="assets/images/products/${destination.image}" alt="${destination.title}" />
+          <img src="assets/images/products/${destination.image}" alt="${
+          destination.title
+        }" />
           <span>${destination.subTitle}</span>
           <h4>${destination.title}</h4>
           <div class="stars">
             ${destination.bonus}
           </div>
-          <h4 class="price">Rp.${Number(destination.price).toLocaleString()}/Pax</h4>
-          <a class="add-to-cart" data-name="${destination.title}" data-price="${destination.price}" data-productid="product${destination.destinationId}" onclick="addToCart('${destination.title}', ${destination.price}, 'product${destination.destinationId}')">
+          <h4 class="price">Rp.${Number(
+            destination.price
+          ).toLocaleString()}/Pax</h4>
+          <a class="add-to-cart" data-name="${destination.title}" data-price="${
+          destination.price
+        }" data-productid="product${
+          destination.destinationId
+        }" onclick="addToCart('${destination.title}', ${
+          destination.price
+        }, 'product${destination.destinationId}')">
           <i class="fa-solid fa-plane" onclick="openNav()"></i>
           </a>
         `;
@@ -276,38 +284,46 @@ document.addEventListener('DOMContentLoaded', function () {
         destinationListElement.appendChild(destinationElement);
       });
     })
-    .catch(error => console.error('Error fetching data:', error));
+    .catch((error) => console.error("Error fetching data:", error));
 });
 // Paket Perjalanan Untuk ditampilkan
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // URL API
-  const apiUrl = 'https://be-2-medan-1-production.up.railway.app/promo';
+  const apiUrl = "https://be-2-medan-1-production.up.railway.app/promo";
 
   // Tangkap elemen HTML dengan id 'destinationList'
-  const destinationListElement = document.getElementById('destinationList2');
+  const destinationListElement = document.getElementById("destinationList2");
 
   // Ambil data dari API
   fetch(apiUrl)
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
       return response.json();
     })
-    .then(data => {
+    .then((data) => {
       // Buat elemen HTML untuk setiap destinasi
-      data.forEach(destination => {
-        const destinationElement = document.createElement('div');
-        destinationElement.classList.add('product-cart');
+      data.forEach((destination) => {
+        const destinationElement = document.createElement("div");
+        destinationElement.classList.add("product-cart");
         destinationElement.innerHTML = `
-          <img src="assets/images/products/${destination.Image}" alt="${destination.title}" />
+          <img src="assets/images/products/${destination.Image}" alt="${
+          destination.title
+        }" />
           <span>${destination.subTitle}</span>
           <h4>${destination.title}</h4>
           <div class="stars">
             ${destination.bonus}
           </div>
-          <h4 class="price">Rp.${Number(destination.price).toLocaleString()}/Pax</h4>
-          <a class="add-to-cart" data-name="${destination.title}" data-price="${destination.price}" data-productid="product${destination.promoId}" onclick="addToCart('${destination.title}', ${destination.price}, 'product${destination.promoId}')">
+          <h4 class="price">Rp.${Number(
+            destination.price
+          ).toLocaleString()}/Pax</h4>
+          <a class="add-to-cart" data-name="${destination.title}" data-price="${
+          destination.price
+        }" data-productid="product${destination.promoId}" onclick="addToCart('${
+          destination.title
+        }', ${destination.price}, 'product${destination.promoId}')">
           <i class="fa-solid fa-plane" onclick="openNav()"></i>
           </a>
         `;
@@ -316,5 +332,5 @@ document.addEventListener('DOMContentLoaded', function () {
         destinationListElement.appendChild(destinationElement);
       });
     })
-    .catch(error => console.error('Error fetching data:', error));
+    .catch((error) => console.error("Error fetching data:", error));
 });
